3d6a41c7598eb419bbdfa24f5513e1ac978f405a
Khushboo <kushbooagarwal3584@gmail.com>
2022-09-18 10:50:12 +0530

    init

--- server.js
+++ server.js
@@ -54,29 +54,6 @@ app.get("/test", (req, res) => {
   res.send("Test Api");
 });
 
-app.get("/video", (req, res) => {
-  const range = req.headers.range;
-  const videoPath = "./GraphQL.mp4";
-  const videoSize = fs.statSync(videoPath).size;
-
-  const chunkSize = 1 * 1e6;
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + chunkSize, videoSize - 1);
-
-  const contentLength = end - start + 1;
-
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
-  console.log(headers);
-  res.writeHead(206, headers);
-
-  const stream = fs.createReadStream(videoPath, { start, end });
-  stream.pipe(res);
-});
 
 app.listen(process.env.PORT || port, () =>
   console.log(`App listening on port ${port}!`)
